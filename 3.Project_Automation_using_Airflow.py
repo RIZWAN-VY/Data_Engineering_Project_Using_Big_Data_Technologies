944ff6317988c30adc7f8595251c36b3d85e0370
RIZWAN VY <rizwanyoosaf37@gmail.com>
2024-04-30 12:17:59 +0530

    added steps

---
+++ 3.Project_Automation_using_Airflow.py
@@ -0,0 +1,17 @@
+'''
+ AUTOMATING THE ENTIRE WORKFLOW USING APACHE AIRFLOW
+=====================================================
+
+Steps :
+    1.Creating a folder in Hadoop HDFS
+    2.Uploading the Dataset to the created folder in HDFS
+    3.Creating a table in Hive which is compactable with the Dataset
+    4.Extracting the data from HDFS to the table created in Hive
+    5.Connecting Hive and Spark and doing analysis using Spark SQL
+        Sales Statistics
+        Top Selling Product
+        Top Product Category
+        Top Performing Sales Representative
+        Sales by City
+    6.Uploading the analysed data back to HDFS
+'''
